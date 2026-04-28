2d94442fc4ba8d34772065d249d21cc008076a5c
mgghellere-jpg <mgghellere@minha.fag.edu.br>
2026-04-28 19:34:23 -0300

    Update QuartoSimples.java

--- PROVA1BIM/QuartoSimples.java
+++ PROVA1BIM/QuartoSimples.java
@@ -1,5 +1,5 @@
 public class QuartoSimples extends Quarto {
-    private boolean temVentilador;//eu nao aguento mais
+    private boolean temVentilador;//quarto simples do hotel
 
     public QuartoSimples(int numero, double valorDiaria, boolean temVentilador) {
         super(numero, valorDiaria);
